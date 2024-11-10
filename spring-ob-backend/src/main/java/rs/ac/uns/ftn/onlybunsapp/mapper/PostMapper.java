8073e5205d6b0efb780793b64e2758c36eee773b
Luka Ivanisevic <lukaivanisevic10@gmail.com>
2024-11-10 20:45:04 +0100

    Added location and post mappers

---
+++ spring-ob-backend/src/main/java/rs/ac/uns/ftn/onlybunsapp/mapper/PostMapper.java
@@ -0,0 +1,18 @@
+package rs.ac.uns.ftn.onlybunsapp.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+import rs.ac.uns.ftn.onlybunsapp.dto.postDtos.LocationDto;
+import rs.ac.uns.ftn.onlybunsapp.dto.postDtos.PostCreateDto;
+import rs.ac.uns.ftn.onlybunsapp.dto.postDtos.PostReadDto;
+import rs.ac.uns.ftn.onlybunsapp.model.Location;
+import rs.ac.uns.ftn.onlybunsapp.model.Post;
+
+@Mapper(componentModel = "spring", uses = {LocationMapper.class})
+public interface PostMapper {
+    Post toPostDomain(PostCreateDto postDto);
+
+    PostReadDto toPostReadDto(Post post);
+}
+
